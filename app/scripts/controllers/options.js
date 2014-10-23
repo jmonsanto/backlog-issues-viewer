155e6102fc8e2d8e3587144652b008bd677ecb5d
mamor <mamor.dev@gmail.com>
2014-10-23 22:17:16 +0900

    複数アカウントに対応した

--- app/scripts/controllers/options.js
+++ app/scripts/controllers/options.js
@@ -10,6 +10,9 @@ angular.module('OptionsApp')
     // authentication user's Backlog data
     $scope.myself = {};
 
+    // multiple accounts
+    $scope.accounts = [];
+
     // Backlog projects
     $scope.projects = [];
 
@@ -24,7 +27,14 @@ angular.module('OptionsApp')
      */
     $scope.saveAuth = function () {
       Backlog.getMySelf($scope.auth).then(function (response) {
-        chrome.storage.sync.set({auth: $scope.auth, myself: response}, self.sync);
+        var account = _.findWhere($scope.accounts, {id: response.id}) ||
+          {id: response.id, auth: angular.copy($scope.auth), myself: angular.copy(response)};
+
+        // remove duplicate account and unshift
+        $scope.accounts = _.without($scope.accounts, account);
+        $scope.accounts.unshift(account);
+
+        chrome.storage.sync.set({auth: $scope.auth, myself: response, accounts: $scope.accounts}, self.sync);
         $scope.getProjects();
 
         $scope.messages.saveAuth = 'Saved!!';
@@ -70,10 +80,29 @@ angular.module('OptionsApp')
     };
 
     /**
+     * @param {number} id
+     */
+    $scope.changeAccount = function (id) {
+      var account = _.findWhere($scope.accounts, {id: id});
+      $scope.auth = angular.copy(account.auth);
+      $scope.saveAuth();
+    };
+
+    /**
+     * @param {number} id
+     */
+    $scope.removeAccount = function (id) {
+      var account = _.findWhere($scope.accounts, {id: id});
+      $scope.accounts = _.without($scope.accounts, account);
+
+      chrome.storage.sync.set({accounts: $scope.accounts}, self.sync);
+    };
+
+    /**
      * sync with chrome storage
      */
     self.sync = function () {
-      var params = {auth: Globals.auth, projects: [], conditions: Globals.conditions, myself: {}};
+      var params = {auth: Globals.auth, projects: [], conditions: Globals.conditions, myself: {}, accounts: []};
 
       chrome.storage.sync.get(params, function (items) {
         $scope.$apply(function () {
@@ -81,6 +110,7 @@ angular.module('OptionsApp')
           $scope.conditions = items.conditions;
           $scope.projects = items.projects;
           $scope.myself = items.myself;
+          $scope.accounts = items.accounts;
         });
       });
 
